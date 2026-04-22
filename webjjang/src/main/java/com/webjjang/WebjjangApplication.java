1fdc343204187d5cd9dc0fe3b0a2e27881508b79
User <User@HYW>
2026-04-22 17:43:57 +0900

    webjjang project Boot 3.5.13

---
+++ webjjang/src/main/java/com/webjjang/WebjjangApplication.java
@@ -0,0 +1,13 @@
+package com.webjjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjjangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebjjangApplication.class, args);
+	}
+
+}
---
+++ webjjang/src/test/java/com/webjjang/WebjjangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webjjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebjjangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
