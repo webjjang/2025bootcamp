c20a1f64854aaa0bbbb8a650d01e768c0e9261e0
User <User@HYW>
2026-04-29 17:46:57 +0900

    MemberController의 로그인 메서드 만들기

---
+++ webjjang/src/main/java/com/webjjang/member/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.webjjang.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.webjjang.member.vo.LoginVO;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/member")
+@Log4j2
+public class MemberController {
+
+	// 1-1. login 폼
+	public String loginForm(Model model) {
+		
+		// 내용에 해당되는 JSP 정보를 contentPage
+		model.addAttribute("contentPage", "../member/loginForm.jsp");
+		// 창이름
+		model.addAttribute("title", "로그인 폼");
+		
+		return "main/main";
+	}
+	
+	// 1-2. login 처리
+	public String login(LoginVO vo) {
+		
+		return "redirect:/board/list.do";
+	}
+	// 2. logout
+	
+}
---
+++ webjjang/src/main/java/com/webjjang/member/vo/LoginVO.java
@@ -0,0 +1,14 @@
+package com.webjjang.member.vo;
+
+import lombok.Data;
+
+@Data
+public class LoginVO {
+
+	private String id;
+	private String pw;
+	private String name;
+	private Integer gradeNo;
+	private String gradeName;
+	
+}
