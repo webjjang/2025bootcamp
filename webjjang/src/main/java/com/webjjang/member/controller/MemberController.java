04fddd9208b10f9d0dca41ba35a6e4f4c2e3066f
User <User@HYW>
2026-04-29 19:59:41 +0900

    로그인 처리리 완료

--- webjjang/src/main/java/com/webjjang/member/controller/MemberController.java
+++ webjjang/src/main/java/com/webjjang/member/controller/MemberController.java
@@ -1,13 +1,17 @@
 package com.webjjang.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.webjjang.member.service.MemberService;
 import com.webjjang.member.vo.LoginVO;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
@@ -15,6 +19,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class MemberController {
 
+	@Autowired
+	private MemberService service;
+	
 	// 1-1. login 폼
 	@GetMapping("/loginForm.do")
 	public String loginForm(Model model) {
@@ -29,8 +36,17 @@ public class MemberController {
 	
 	// 1-2. login 처리
 	@PostMapping("/login.do")
-	public String login(LoginVO vo) {
+	public String login(LoginVO vo, HttpSession session, RedirectAttributes rttr) {
 		
+		// 아이디와 비밀번호가 맞으면 LoginVO 객체가 저장 - 데이터 존재
+		// 맞지 않으면 null 이 저장 - 데이터가 존재하지 않는다. - empty
+		session.setAttribute("login", service.login(vo));
+		if(session.getAttribute("login") == null) {
+			rttr.addFlashAttribute("msg", "로그인 오류<hr>정보를 확인하고 다시 시도해 주세요.");
+			return "redirect:/member/loginForm.do";
+		}
+		
+		rttr.addFlashAttribute("msg", "로그인이 되었습니다.<hr>즐거운 시간되세요.");
 		return "redirect:/board/list.do";
 	}
 	// 2. logout
---
+++ webjjang/src/main/java/com/webjjang/member/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.webjjang.member.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webjjang.member.mapper.MemberMapper;
+import com.webjjang.member.vo.LoginVO;
+
+@Service
+public class MemberService {
+
+	@Autowired
+	private MemberMapper mapper;
+	
+	// 1. login
+	public LoginVO login(LoginVO vo) {
+		return mapper.login(vo);
+	}
+	
+}
