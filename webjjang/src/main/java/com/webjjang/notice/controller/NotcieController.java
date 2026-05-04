523b39187e0e14aaabad4e332d5b4af890b76a87
User <User@HYW>
2026-05-04 20:27:54 +0900

    Nitce Controller URL 작성

---
+++ webjjang/src/main/java/com/webjjang/notice/controller/NotcieController.java
@@ -0,0 +1,103 @@
+package com.webjjang.notice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/notice")
+@Log4j2
+public class NotcieController {
+
+	// 1.list
+	@GetMapping("/list.do")
+	public String list(Model model) {
+		
+		// 내용에 해당되는 JSP 정보를 contentPage
+		model.addAttribute("contentPage", "../notice/list.jsp");
+		// 창이름
+		model.addAttribute("title", "공지 리스트");
+				
+		return "main/main";
+	}
+	
+	// 2. view
+	@GetMapping("/view.do")
+	public String view(Model model) {
+		
+		// 내용에 해당되는 JSP 정보를 contentPage
+		model.addAttribute("contentPage", "../notice/view.jsp");
+		// 창이름
+		model.addAttribute("title", "공지 상세보기");
+				
+		return "main/main";
+	}
+	
+	
+	// 3. writeForm
+	@GetMapping("/writeForm.do")
+	public String writeForm(Model model) {
+		
+		// 내용에 해당되는 JSP 정보를 contentPage
+		model.addAttribute("contentPage", "../notice/writeForm.jsp");
+		// 창이름
+		model.addAttribute("title", "공지 등록");
+				
+		return "main/main";
+	}
+	
+	// 4. write
+	@PostMapping("/write.do")
+	public String write(RedirectAttributes rttr) {
+		
+		// 메시지 처리
+		rttr.addFlashAttribute("msg", "새로운 공지가 등록되었습니다.");
+				
+		return "redirect:list.do";
+	}
+	
+	
+	// 5. updateForm
+	@GetMapping("/updateForm.do")
+	public String updateForm(Model model) {
+		
+		// 내용에 해당되는 JSP 정보를 contentPage
+		model.addAttribute("contentPage", "../notice/updateForm.jsp");
+		// 창이름
+		model.addAttribute("title", "공지 수정");
+				
+		return "main/main";
+	}
+	
+	// 6. update
+	@PostMapping("/update.do")
+	public String update(RedirectAttributes rttr) {
+		
+		// 수정이 되었는지 결과에 따라 처리가 다름 if
+		
+		// 메시지 처리
+		rttr.addFlashAttribute("msg", "새로운 공지가 등록되었습니다.");
+				
+		return "redirect:list.do";
+	}
+	
+	
+	// 7. delete
+	@GetMapping("/delete.do")
+	public String delete(RedirectAttributes rttr) {
+		
+		// 수정이 되었는지 결과에 따라 처리가 다름 if
+		
+		// 메시지 처리
+		rttr.addFlashAttribute("msg", "공지가 삭제되었습니다.");
+				
+		return "redirect:list.do";
+	}
+	
+	
+}
--- webjjang/src/main/webapp/WEB-INF/views/member/list.jsp
+++ webjjang/src/main/webapp/WEB-INF/views/member/list.jsp
@@ -33,13 +33,9 @@
 	 	// 글번호 수집
 	 	// text() - 글자만 가져온다. html() - tag도 가져온다. :: jQuery
 	 	// js의 변수는 타입이 없다. - 변수 = 10 - 선언 없이 바로 사용가능. var로 변수 선언. let으로 변수 선언.-지역변수 구분 확실
-	 	let no = $(this).find(".no").text(); // js = jQuery
-	 	// alert("클릭한 글번호 : " + no); // js
-	 	// 페이지 이동 시키기 - 브라우저 객체 중 location 객체가 있다. 보여지는 페이지들의 정보를 가지고 있는 객체
-	 	// location 객체 - BOM 객체 중에 하나.
-	 	// location.href = "view.jsp?no=" + no; // location = "url" == location.href = "url"
+	 	let id = $(this).find(".id").text(); // js = jQuery
 	 	// location = "url" : 자동으로 location.href에 들어간다.
-	 	location = "view.do?no=" + no + "&inc=1";
+	 	location = "view.do?id=" + id;
 	 }).mouseover(function(){
 		 $(this).addClass("table-success");
 	 }).mouseout(function(){
