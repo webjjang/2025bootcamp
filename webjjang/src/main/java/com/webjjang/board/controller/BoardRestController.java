c122857e2a2fc164b16e886838f16f92e0c74291
User <User@HYW>
2026-05-21 21:41:10 +0900

    BoardRestController 사용하지 않는 import 제거

--- webjjang/src/main/java/com/webjjang/board/controller/BoardRestController.java
+++ webjjang/src/main/java/com/webjjang/board/controller/BoardRestController.java
@@ -8,15 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.webjjang.board.service.BoardService;
 import com.webjjang.board.vo.BoardVO;
