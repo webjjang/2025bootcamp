457c9121a7049c145d5d5fb424f6f142798e00b6
User <User@HYW>
2026-05-21 21:38:46 +0900

    일반게시판 리스트 API JSON 데이터 제공

---
+++ webjjang/src/main/java/com/webjjang/board/controller/BoardRestController.java
@@ -0,0 +1,71 @@
+package com.webjjang.board.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.webjjang.board.service.BoardService;
+import com.webjjang.board.vo.BoardVO;
+import com.webjjang.util.page.PageObject;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+
+// 자동생성되는 어노테이션
+// - @Controller, @Service, @Mapper, @Repository, @Component, @RestController, @~Advice
+@RestController
+@RequestMapping("/boardApi")
+@CrossOrigin(origins = "*") // Spring Boot 프로젝트(80포트)와 React 프록젝트(5173)는 서로 다른 것으로 인식한다.
+@Log4j2
+public class BoardRestController {
+
+	// 생성된 객체를 자동 DI 시키는 @Autowired 스프링 어노테이션 사용
+	@Autowired
+	private BoardService service;
+	
+	//1. 리스트
+	@GetMapping(value = "/list.do",
+			// 넘겨 주는 데이터 타입
+			produces = {
+				MediaType.APPLICATION_JSON_VALUE
+		})
+//	public String list(Long page, Model model) 
+	public ResponseEntity<Map<String, Object>> list(HttpServletRequest request) 
+	throws Exception {
+		
+		log.info("일반게시판 API 리스트");
+		// @Log : log.info(str), @Log4j2 : log.info(Object)
+		// log.info(page);
+		// PageObject을 이용해서 넘어오는 페이지 정보와 검색 정보를 받는다.
+		PageObject pageObject = PageObject.getInstance(request);
+		
+		// 넘겨 주는 데이터 변수 - 댓글 리스트 데이터 + PageObject
+		Map<String, Object> map = new HashMap<>();
+		
+		// 서비스 실행해서 결과를 변수에 담는다.
+		List<BoardVO> list = service.list(pageObject);
+		
+		map.put("list", list);
+		// pageObjcet
+		map.put("pageObject", pageObject);
+				
+		return  new ResponseEntity<>(map, HttpStatus.OK);
+	}
+	
+	
+	
+}
--- webjjang/src/main/java/com/webjjang/board/vo/BoardVO.java
+++ webjjang/src/main/java/com/webjjang/board/vo/BoardVO.java
@@ -2,6 +2,8 @@ package com.webjjang.board.vo;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,6 +13,7 @@ public class BoardVO {
 	private String title;
 	private String content;
 	private String writer;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date writeDate;// java.sql.Date -> java.util.Date : Spring에서는 자동 변환해서 담는다.
 	private Long hit;
 	private String pw;
